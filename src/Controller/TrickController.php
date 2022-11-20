5b1bbe1885d3242553d4ddf28b158f2bc9e5325c
gregdev <gvandevray@numericable.fr>
2022-11-20 21:03:42 +0100

    Create TrickController

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -2,20 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Trick;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class TrickController extends AbstractController
 {
-    #[Route('/trick', name: 'app_trick')]
-    public function index(TranslatorInterface $translator): Response
+    #[Route('/', name: 'home')]
+    public function index(ManagerRegistry $doctrine): Response
     {
-        $hello = $translator->trans('snow_trick.hello');
+
+        $tricks = $doctrine->getRepository(Trick::class)->findAll();
         return $this->render('trick/index.html.twig', [
-            'controller_name' => 'TrickController',
-            'hello' => $hello
+            'tricks' => $tricks
         ]);
     }
 }
