b42031121663363f44dc3d6b6f3b6a9f342ab559
gregdev <gvandevray@numericable.fr>
2023-01-12 17:38:44 +0100

    ending template trick detail css responsive

--- src/Controller/Trick/TrickController.php
+++ src/Controller/Trick/TrickController.php
@@ -2,23 +2,54 @@
 
 namespace App\Controller\Trick;
 
+use App\Entity\Comment;
+use App\Entity\User;
+use App\Form\CommentFormType;
+use App\Paginator\PaginatorService;
+use App\Repository\CommentRepository;
 use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Uid\Uuid;
 
 class TrickController extends AbstractController
 {
     #[Route('/trick/{slug}', name: 'app_trick')]
     public function __invoke(
         string $slug,
-        TrickRepository $trickRepository
+        TrickRepository $trickRepository,
+        CommentRepository $commentRepository,
+        Request $request,
+        PaginatorService $paginatorService
     ): Response {
+        $page = $request->query->get('page') ?? 1;
         $trick = $trickRepository->findOneBy(['slug' => $slug]);
-        return $this->render('trick/trick/index.html.twig', [
+        if (!$trick) {
+            throw new NotFoundHttpException();
+        }
+        $comment = new Comment();
+        $form = $this->createForm(CommentFormType::class, $comment);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var User $user */
+            $user = $this->getUser();
+            $comment->setUser($user);
+            $comment->setTrick($trick);
+            $commentRepository->save($comment, true);
+            $comment = new Comment();
+            $form = $this->createForm(CommentFormType::class, $comment);
+        }
+        $comments = $paginatorService->paginateComment($trick, $page);
+        return $this->render('trick/trick_detail/index.html.twig', [
             'trick' => $trick,
+            'comments' => $comments,
+            'form' => $form->createView(),
             'add_header' => true,
-            'fix_footer' => true
+            'fix_footer' => true,
+            'page' => $page
         ]);
     }
 }
