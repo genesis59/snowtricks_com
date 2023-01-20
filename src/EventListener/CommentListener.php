b2b5fc6eee3dd4e41e02e2b46bd5672d3b40aeba
gregdev <gvandevray@numericable.fr>
2023-01-20 11:42:53 +0100

    add comment form and features to manage it

--- src/EventListener/CommentListener.php
+++ src/EventListener/CommentListener.php
@@ -3,16 +3,23 @@
 namespace App\EventListener;
 
 use App\Entity\Comment;
+use App\Entity\User;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Uid\Uuid;
 
 class CommentListener
 {
-    public function __construct()
+    public function __construct(private readonly Security $security)
     {
     }
     public function prePersist(Comment $comment, LifecycleEventArgs $args): void
     {
+        if ($this->security->getUser()) {
+            /** @var User $user */
+            $user = $this->security->getUser();
+            $comment->setUser($user);
+        }
         $comment->setUuid(Uuid::v4());
         $comment->setCreatedAt(new \DateTimeImmutable());
     }
