f8100183e6b27620737f99b89aea2c70fe574b56
gregdev <gvandevray@numericable.fr>
2023-02-24 15:22:18 +0100

    Codacy clean unused code

--- src/Controller/User/UserActivationNewController.php
+++ src/Controller/User/UserActivationNewController.php
@@ -5,7 +5,6 @@ namespace App\Controller\User;
 use App\Entity\User;
 use App\Event\UserEmailEvent;
 use App\Form\NewActivationFormType;
-use App\Mailer\MailerService;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
--- src/Controller/User/UserCreateController.php
+++ src/Controller/User/UserCreateController.php
@@ -2,22 +2,16 @@
 
 namespace App\Controller\User;
 
-use App\Entity\Photo;
 use App\Entity\User;
 use App\Event\UserEmailEvent;
 use App\Form\UserFormType;
-use App\Mailer\MailerService;
 use App\Repository\UserRepository;
 use App\UploadService\UploadService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
-use Symfony\Component\Uid\Uuid;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class UserCreateController extends AbstractController
@@ -26,11 +20,8 @@ class UserCreateController extends AbstractController
     public function __invoke(
         Request $request,
         UserRepository $userRepository,
-        UserPasswordHasherInterface $passwordHasher,
         TranslatorInterface $translator,
         EventDispatcherInterface $dispatcher,
-        TokenGeneratorInterface $tokenGenerator,
-        MailerService $mailerService,
         UploadService $uploadService
     ): Response {
         if ($this->getUser()) {
